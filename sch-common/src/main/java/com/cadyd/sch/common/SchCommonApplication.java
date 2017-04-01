03e50ad97aaebcc507019d4a82ef79f833a2826b
qeprwjcgya <158921300@qq.com>
2017-04-01 21:48:16 +0800

    提交到本地仓库

---
+++ sch-common/src/main/java/com/cadyd/sch/common/SchCommonApplication.java
@@ -0,0 +1,12 @@
+package com.cadyd.sch.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchCommonApplication.class, args);
+	}
+}
